58a5daf0fcc1f7bb4b9a6d325d6b5a145eb91380
Simon Reich <sreich@gwdg.de>
2014-12-27 23:54:33 +0100

    auswertung.py erstelleRekordeBeispiel minor changes

--- auswertung.py
+++ auswertung.py
@@ -421,10 +421,12 @@ def erstelleRekordeBeispiel():
     print("Dort befinden sich Beispiele für die Datei " + fileInputRekorde + ".")
     dataOutput = [["Altersklasse", "WK1", "WK2", "WK3", "WK4", "WK5"], 
         ["5/6",10,11,12,13,14], 
-        ["7/10",8.9,7.6,4.5,11.5,30.4], 
-        ["11/14",12.4,45.5,14.5,17.6,12.4], 
+        ["7/8",8.9,7.6,4.5,11.5,30.4], 
+        ["9/10",8.9,7.6,4.5,11.5,30.4], 
+        ["11/12",12.4,45.5,14.5,17.6,12.4], 
+        ["13/14",12.4,45.5,14.5,17.6,12.4], 
         ["15/16",10.4,12.3,14.4,15.6,17.5], 
-        ["17/10",12.3,14.5,15.6,16.7,18.9], 
+        ["17/18",12.3,14.5,15.6,16.7,18.9], 
         ["offen",20,20,20,20,40]]
     fileOutputHandle = open (fileOutputRekordeBeispiel, "wt")
     fileOutputWriter = csv.writer (fileOutputHandle, 
